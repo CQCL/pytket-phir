c2eb07831abbc9432d9d1da75e12205349bde3a7
Neal Erickson <neal.erickson@quantinuum.com>
2023-09-28 08:29:31 -0600

    removing headers

--- pytket/phir/sharding/sharder.py
+++ pytket/phir/sharding/sharder.py
@@ -1,14 +1,3 @@
-##############################################################################
-#
-# (c) 2023 @ Quantinuum LLC. All Rights Reserved.
-# This software and all information and expression are the property of
-# Quantinuum LLC, are Quantinuum LLC Confidential & Proprietary,
-# contain trade secrets and may not, in whole or in part, be licensed,
-# used, duplicated, disclosed, or reproduced for any purpose without prior
-# written permission of Quantinuum LLC.
-#
-##############################################################################
-
 from pytket.circuit import Circuit, Command, Op, OpType
 from pytket.unit_id import UnitID
 
--- tests/test_sharder.py
+++ tests/test_sharder.py
@@ -1,14 +1,6 @@
 ##############################################################################
 #
 # (c) 2023 @ Quantinuum LLC. All Rights Reserved.
-# This software and all information and expression are the property of
-# Quantinuum LLC, are Quantinuum LLC Confidential & Proprietary,
-# contain trade secrets and may not, in whole or in part, be licensed,
-# used, duplicated, disclosed, or reproduced for any purpose without prior
-# written permission of Quantinuum LLC.
-#
-##############################################################################
-
 from pytket.phir.sharding.sharder import Sharder
 
 from .sample_data import QasmFiles, get_qasm_as_circuit
