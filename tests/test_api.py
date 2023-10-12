46e34af531d475d084439e1023fc61efe6e45a9d
Neal Erickson <neal.erickson@quantinuum.com>
2023-10-12 10:59:02 -0600

    adding test for h1-1

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,5 @@
 from pytket.phir.api import pytket_to_phir
+from pytket.phir.qtm_machine import QtmMachine
 
 from .sample_data import QasmFiles, get_qasm_as_circuit
 
@@ -8,6 +9,15 @@ class TestApi:
         circuit = get_qasm_as_circuit(QasmFiles.baby)
 
         phir = pytket_to_phir(circuit)
+
+        # TODO: Make this test more valuable once PHIR is actually returned
+        print(phir)
+        assert len(phir) > 0
+
+    def test_pytket_to_phir_h1_1(self) -> None:
+        circuit = get_qasm_as_circuit(QasmFiles.baby)
+
+        phir = pytket_to_phir(circuit, QtmMachine.H1_1)
 
         # TODO: Make this test more valuable once PHIR is actually returned
         print(phir)
