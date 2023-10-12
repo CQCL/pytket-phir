c1a25ce29454f4d5e595af3913bae9a5a2bc8bd8
Neal Erickson <neal.erickson@quantinuum.com>
2023-10-12 10:20:06 -0600

    ruff cleanup

--- pytket/phir/api.py
+++ pytket/phir/api.py
@@ -6,11 +6,10 @@ from .sharding.sharder import Sharder
 def pytket_to_phir(
     circuit: Circuit,
 ) -> str:
-    """Converts a pytket circuit into its PHIR representation
+    """Converts a pytket circuit into its PHIR representation.
 
     :param circuit: Circuit object to be converted
     """
-
     # TODO: Process circuit according to desired machine architecture
 
     sharder = Sharder(circuit)
