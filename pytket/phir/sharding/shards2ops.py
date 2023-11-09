ac1789b0ec70d89c485463b595497b127d4eac22
Neal Erickson <minus.twelve@gmail.com>
2023-11-09 09:54:03 -0700

    quick try (#27)
    Co-authored-by: Neal Erickson <neal.erickson@quantinuum.com>

--- pytket/phir/sharding/shards2ops.py
+++ pytket/phir/sharding/shards2ops.py
@@ -13,16 +13,15 @@ def parse_shards_naive(
     while len(scheduled) < num_shards:
         layer: list[list[int]] = []
         to_schedule: list[Shard] = []
-        # get all the shards with no dependencies
+        # Iterate the shards, looking for shards whose dependencies have been
+        # satisfied, or initially, shards with no dependencies
         for shard in shards:
-            s = shard
-            deps = s.depends_upon
-            # dependencies of the shard that have already been scheduled
-            scheduled_deps = deps.intersection(scheduled)
-            already_scheduled = s.ID in scheduled
-
-            if scheduled_deps == deps and not already_scheduled:
-                to_schedule.append(s)
+            if shard.ID not in scheduled:
+                deps = shard.depends_upon
+                # dependencies of the shard that have already been scheduled
+                scheduled_deps = deps.intersection(scheduled)
+                if scheduled_deps == deps:
+                    to_schedule.append(shard)
         shards_in_layer.append(to_schedule)
 
         for shard in to_schedule:
