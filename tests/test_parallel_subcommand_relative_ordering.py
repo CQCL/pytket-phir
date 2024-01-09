eafa660a74cfed69e59f40ab7c93eee4eef52487
Asa-Kosto-QTM <108833721+Asa-Kosto-QTM@users.noreply.github.com>
2024-01-09 16:52:11 -0500

    Update tests/test_parallel_subcommand_relative_ordering.py
    Co-authored-by: Kartik Singhal <kartik.singhal@quantinuum.com>

--- tests/test_parallel_subcommand_relative_ordering.py
+++ tests/test_parallel_subcommand_relative_ordering.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2023 Quantinuum LLC All rights reserved.
+# Copyright (c) 2024 Quantinuum LLC All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 #
