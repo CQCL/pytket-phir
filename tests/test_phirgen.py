7d6311584c8886111742fb3f3e67bec10e112921
Kartik Singhal <kartik.singhal@quantinuum.com>
2024-01-20 07:24:15 +0000

    test: ensure issue links are clickable

--- tests/test_phirgen.py
+++ tests/test_phirgen.py
@@ -16,7 +16,7 @@ from pytket.phir.api import pytket_to_phir
 
 class TestPhirGen:
     def test_classicalexpbox(self) -> None:
-        """From https://github.com/CQCL/pytket-phir/issues/86."""
+        """From https://github.com/CQCL/pytket-phir/issues/86 ."""
         circ = Circuit(1)
         a = circ.add_c_register("a", 2)
         b = circ.add_c_register("b", 2)
@@ -31,7 +31,7 @@ class TestPhirGen:
         }
 
     def test_nested_arith(self) -> None:
-        """From https://github.com/CQCL/pytket-phir/issues/87."""
+        """From https://github.com/CQCL/pytket-phir/issues/87 ."""
         circ = Circuit(1)
         a = circ.add_c_register("a", 2)
         b = circ.add_c_register("b", 2)
@@ -46,7 +46,7 @@ class TestPhirGen:
         }
 
     def test_arith_with_int(self) -> None:
-        """From https://github.com/CQCL/pytket-phir/issues/88."""
+        """From https://github.com/CQCL/pytket-phir/issues/88 ."""
         circ = Circuit(1)
         a = circ.add_c_register("a", 2)
         circ.add_classicalexpbox_register(a << 1, a.to_list())
@@ -59,7 +59,7 @@ class TestPhirGen:
         }
 
     def test_bitwise_ops(self) -> None:
-        """From https://github.com/CQCL/pytket-phir/issues/91."""
+        """From https://github.com/CQCL/pytket-phir/issues/91 ."""
         circ = Circuit(1)
         a = circ.add_c_register("a", 2)
         b = circ.add_c_register("b", 2)
