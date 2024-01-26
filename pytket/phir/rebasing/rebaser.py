0a83af650c763dfa91816d9deb288587cf201449
Alec Edgington <alec.edgington@quantinuum.com>
2024-01-26 08:38:08 +0000

    Refactor.

--- pytket/phir/rebasing/rebaser.py
+++ pytket/phir/rebasing/rebaser.py
@@ -8,10 +8,9 @@
 
 from typing import TYPE_CHECKING
 
-from pytket.circuit import OpType
 from pytket.passes import DecomposeBoxes
 from pytket.passes.auto_rebase import auto_rebase_pass
-from pytket.phir.qtm_machine import QTM_MACHINES_MAP, QtmMachine
+from pytket.phir.qtm_machine import QTM_DEFAULT_GATESET, QTM_MACHINES_MAP, QtmMachine
 
 if TYPE_CHECKING:
     from pytket.circuit import Circuit
@@ -20,11 +19,7 @@ if TYPE_CHECKING:
 def rebase_to_qtm_machine(circuit: "Circuit", qtm_machine: QtmMachine) -> "Circuit":
     """Rebases a circuit's gate to the gate set appropriate for the given machine."""
     machine = QTM_MACHINES_MAP.get(qtm_machine)
-    gateset = (
-        {OpType.Rz, OpType.PhasedX, OpType.ZZPhase}
-        if machine is None
-        else machine.gateset
-    )
+    gateset = QTM_DEFAULT_GATESET if machine is None else machine.gateset
     c = circuit.copy()
     DecomposeBoxes().apply(c)
     auto_rebase_pass(gateset, allow_swaps=True).apply(c)
