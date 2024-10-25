f98da8d1ad19ce2f3353b8bd5f5ca037f304e8a2
Kartik Singhal <kartik.singhal@quantinuum.com>
2024-10-25 07:45:47 -0500

    fix: lint updates

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -12,9 +12,9 @@ EXTENSION_NAME="$(basename "$(dirname `pwd`)")"
 sed -i '' 's#CQCL/tket#CQCL/'$EXTENSION_NAME'#' _static/nav-config.js
 
 # Build the docs. Ensure we have the correct project title.
-sphinx-build -b html -D html_title="$EXTENSION_NAME" . build 
+sphinx-build -b html -D html_title="$EXTENSION_NAME" . build
 
 # Remove copied files. This ensures reusability.
-rm -r _static 
+rm -r _static
 rm -r quantinuum-sphinx
 rm conf.py
